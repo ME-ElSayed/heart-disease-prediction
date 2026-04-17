50610cad06204a79381c9f16448478c6c929b030
mahmoud123asd <https://github.com/mahmoud123asd>
2026-04-17 23:16:20 +0200

    Refactor PredictResponse to correctly parse prediction data and include top features

--- lib/features/prediction/data/model/predict_response.dart
+++ lib/features/prediction/data/model/predict_response.dart
@@ -1,16 +1,23 @@
+import 'package:heart_disease_prediction/features/prediction/data/model/top_features.dart';
+
 class PredictResponse {
   final bool hasHeartDisease;
   final double probability;
+  final List<TopFeature> topFeatures;
 
   const PredictResponse({
     required this.hasHeartDisease,
     required this.probability,
+    required this.topFeatures,
   });
 
   factory PredictResponse.fromJson(Map<String, dynamic> json) {
     return PredictResponse(
-      hasHeartDisease: json['has_heart_disease'] as bool,
+      hasHeartDisease: (json['prediction'] as int) == 1,
       probability: (json['probability'] as num).toDouble(),
+      topFeatures: (json['top_features'] as List<dynamic>)
+          .map((e) => TopFeature.fromJson(e as Map<String, dynamic>))
+          .toList(),
     );
   }
 }
